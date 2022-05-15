5be565c8d98fbc1a50380f9c4d1b38cb77ab55d5
tuimac <tuimac.devadm01@gmail.com>
2022-05-15 22:49:46 +0900

    fixed the indent for Python code on Lambda

--- cloudformation/launch.sh
+++ cloudformation/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STACK_NAME='containerRegistry4'
+STACK_NAME='containerRegistry5'
 REGION='ap-northeast-3'
 
 aws cloudformation create-stack --stack-name $STACK_NAME --region ${REGION} --capabilities CAPABILITY_NAMED_IAM --template-body file://template.yml
